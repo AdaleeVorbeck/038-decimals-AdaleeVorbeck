15f59b27957f1d5826051821816a662d00248370
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-11-12 20:15:10 +0000

    take 4!

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let lengthInFeet = 1.5;
 let pricePerFoot = 2.50;
 let totalPrice = lengthInFeet * pricePerFoot;
 
-document.getElementById("result").innerHTML = totalPrice;
+document.getElementById("result").innerHTML = totalPrice.toFixed(2);
